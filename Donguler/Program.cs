cece8146c8e5bdeed6b8b19bb5b2cb54100a7669
BELMA TAŞ <tasbelma.01@gmail.com>
2021-02-16 16:12:46 +0300

    ders sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yteiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yteiştirme Kampı", "Programlamaya başlangıç için temel kurs","Java" };           
+               
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            
+            
+            
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
